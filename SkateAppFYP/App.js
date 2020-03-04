d9d873676482c242f8fbc9c4f13b8c9c70af516c
(s) Jack Williams 1 <jwilliams8@PM7-0671-mac.socnet.soc.plymouth.ac.uk>
2020-03-04 18:07:47 +0000

    refactored the login screen as sizing is incorrect on different devices. moved to %

--- SkateAppFYP/App.js
+++ SkateAppFYP/App.js
@@ -1,82 +1,8 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, TextInput, Button, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, TextInput, Button, TouchableOpacity, Dimensions } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 30,
-    flex: 1
-  },
-  title: {
-    fontSize: 24,
-    textAlign: "center"
-  },
-  titleContainer: {
-    top: 50
-  },
-  taglineContainer: {
-    top: 90
-  },
-  tagline: {
-    textAlign: "center"
-  },
-  appDescripterContainer: {
-    top: 120,
-  },
-  appDescripter: {
-    textAlign: "center"
-  },
-  textInput: {
-    height: 40,
-    paddingLeft: 10
-  },
-  emailContainer: {
-    top: 170,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  passwordContainer: {
-    top: 200,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  inputError: {
-    borderWidth: 2,
-    borderColor: 'red',
-  },
-  forgotPassword: {
-    position: "absolute",
-    right: 0,
-    top: 210,
-    color: "blue",
-  },
-  loginContainer: {
-    top: 260
-  },
-  loginButton: {
-
-  },
-  connectTextContainer: {
-    top: 300,
-  },
-  connectText: {
-    textAlign: "center",
-  },
-  googleContainer: {
-    top: 330,
-  },
-  googleButton: {
-
-  },
-  signUpHereText: {
-    position: "absolute",
-    right: 0,
-    color: "blue"
-  },
-  dontHaveAccountContainer: {
-    marginTop: 40,
-    flexDirection: 'row',
-  }
-});
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
 
 export default class App extends React.Component {
   constructor(props) {
@@ -136,56 +62,110 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar barStyle="white" backgroundColor="black" />
+      <View style={{ flex: 1 }}>
+        {/* <StatusBar barStyle="white" backgroundColor="black" /> */}
         <SafeAreaView>
-            <View style={styles.titleContainer}>
-              <Text style={styles.title}>Skate App</Text>
-            </View>
-            <View style={styles.taglineContainer}>
-              <Text style={styles.tagline}>A community skate application</Text>
-            </View>
-            <View style={styles.appDescripterContainer}>
-              <Text style={styles.appDescripter}>Learn to skate, meet others who skate, teach others to skate.</Text>
-            </View>
-            <View style={!this.state.inputError ? styles.emailContainer : [styles.emailContainer, styles.inputError]}>
+          <ScrollView style={styles.container}>
+
+            <Text style={styles.title}>Skate App</Text>
+            <Text style={styles.tagline}>A community skate application</Text>
+            <Text style={styles.appDescripter}>Learn to skate, meet others who skate, teach others to skate.</Text>
+
+            <View style={styles.textInputContainer}>
               <TextInput
-                style={styles.textInput}
+                style={!this.state.inputError ? styles.textInput : [styles.textInput, styles.inputError]}
                 onChangeText={email => this.setEmail(email)}
                 placeholder="Email"
                 autoCompleteType="email"
               />
-            </View>
-            <View style={!this.state.inputError ? styles.passwordContainer : [styles.passwordContainer, styles.inputError]}>
               <TextInput
-                style={styles.textInput}
+                style={!this.state.inputError ? styles.textInput : [styles.textInput, styles.inputError]}
                 onChangeText={password => this.setPassword(password)}
                 placeholder="Password"
                 autoCompleteType="password"
               />
             </View>
-            <View>
-              <TouchableOpacity style={styles.forgotPassword}>
-                <Text style={{ color: 'blue' }}>forgot password?</Text>
+
+            <TouchableOpacity style={styles.forgotPasswordContainer}>
+              <Text style={styles.forgotPassword}>forgot password?</Text>
+            </TouchableOpacity>
+
+            <View style={{ paddingTop: '10%', }}>
+              <Button title="Sign in"></Button>
+              <Text style={styles.connectText}>or connect using</Text>
+              <Button title="Google+"></Button>
+            </View>
+
+          </ScrollView>
+
+          <View style={styles.dontHaveAccountContainer}>
+            <View style={{ flex: 1, textAlign: "center", alignItems: "center" }}>
+              <Text>Don't have an account?</Text>
+              <TouchableOpacity>
+                <Text style={styles.signUpHereText}> Sign up here.</Text>
               </TouchableOpacity>
             </View>
-            <View style={styles.loginContainer}>
-              <Button style={styles.loginButton} title="Sign in"></Button>
-            </View>
-            <View style={styles.connectTextContainer}>
-              <Text style={styles.connectText}>or connect using</Text>
-            </View>
-            <View style={styles.googleContainer}>
-              <Button style={styles.googleButton} title="Google+"></Button>
-              <View style={styles.dontHaveAccountContainer}>
-                <Text>Don't have an account?</Text>
-                <TouchableOpacity style={styles.signUpHereText}>
-                  <Text style={{ color: "blue" }}>Sign up here.</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+          </View>
+
         </SafeAreaView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    paddingTop: '10%'
+  },
+  tagline: {
+    textAlign: "center",
+    paddingTop: '10%'
+  },
+  appDescripter: {
+    paddingTop: '10%',
+    textAlign: "center"
+  },
+  textInputContainer: {
+    paddingTop: '10%',
+  },
+  textInput: {
+    height: 40,
+    paddingLeft: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10
+  },
+  inputError: {
+    height: 40,
+    paddingLeft: 10,
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  forgotPasswordContainer: {
+    alignSelf: 'flex-end',
+    textAlign: 'right',
+  },
+  forgotPassword: {
+    color: 'blue'
+  },
+  connectText: {
+    paddingVertical: 10,
+    textAlign: "center"
+  },
+  signUpHereText: {
+    color: "blue"
+  },
+  dontHaveAccountContainer: {
+    flexDirection: "row",
+    position: 'absolute',
+    bottom: 0,
+    margin: 36
+  }
+});
--- SkateAppFYP/index.js
+++ SkateAppFYP/index.js
@@ -1,9 +1,25 @@
 /**
  * @format
  */
-
+//import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+
+// const Stack = createStackNavigator();
+
+{/* <NavigationContainer>
+<Stack.Navigator>
+  <Stack.Screen
+    name="Home"
+    component={Home}
+    options={{title: 'Welcome'}}
+  />
+  <Stack.Screen name="Profile" component={Profile} />
+</Stack.Navigator>
+</NavigationContainer> */}
 
 AppRegistry.registerComponent(appName, () => App);
