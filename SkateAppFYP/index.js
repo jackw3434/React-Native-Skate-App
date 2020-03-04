e97b9dd881c06b4ca53e41aca58d90b2593ecaf4
(s) Jack Williams 1 <jwilliams8@PM7-0671-mac.socnet.soc.plymouth.ac.uk>
2020-03-04 18:15:46 +0000

    commented stack navigater as crashing app

--- SkateAppFYP/index.js
+++ SkateAppFYP/index.js
@@ -2,24 +2,25 @@
  * @format
  */
 //import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import Login from './pages/loginPage'
+import { name as appName } from './app.json';
 
 // import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
 
 // const Stack = createStackNavigator();
 
-{/* <NavigationContainer>
-<Stack.Navigator>
-  <Stack.Screen
-    name="Home"
-    component={Home}
-    options={{title: 'Welcome'}}
-  />
-  <Stack.Screen name="Profile" component={Profile} />
-</Stack.Navigator>
-</NavigationContainer> */}
+// <NavigationContainer>
+//     <Stack.Navigator>
+//         <Stack.Screen
+//             name="Home"
+//             component={Login}
+//             options={{ title: 'Welcome' }}
+//         />
+//         {/* <Stack.Screen name="Profile" component={Profile} /> */}
+//     </Stack.Navigator>
+// </NavigationContainer>
 
 AppRegistry.registerComponent(appName, () => App);
